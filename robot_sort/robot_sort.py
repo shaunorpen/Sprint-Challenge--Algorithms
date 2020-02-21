e4fbf4079b4a3db28052fd319f1588cdef7e1984
Shaun Orpen <shaunorpen@users.noreply.github.com>
2020-02-21 12:47:09 +0000

    Almost working

--- robot_sort/robot_sort.py
+++ robot_sort/robot_sort.py
@@ -96,68 +96,32 @@ class SortingRobot:
         """
         Sort the robot's list.
         """
-        # # Pick up the first item
-        # self.swap_item()
-        # # Compare it to the next item, keep the biggest each time
-        # while self.move_right():
-        #     if self.compare_item() < 1:
-        #         self.swap_item()
-        # # Until you reach the end of the list, then swap with the item at the end
-        # self.swap_item()
-        # # Compare it to the next item, keep the smallest each time
-        # while self.move_left() and not self.compare_item() == None:
-        #     if self.compare_item() > 0:
-        #         self.swap_item()
-        # # Until you reach None, then drop the item, move right and pick up the next item
-        # # Compare it to the next item, keep the biggest each time
-        # self.swap_item()
-        # self.move_right()
-        # self.swap_item()
-        # while self.move_right():
-        #     if self.can_move_right():
-        #         if self.compare_item() < 1:
-        #             self.swap_item()
-        # # Until you reach the end of the list. Then turn on the light.
-        # self.set_light_on()
-        # # Then compare the item carrying to the item at current position until you find 
-        # # the first value less than the value you're carrying. Swap. Turn off light.
-        # while self.move_left() and self.light_is_on() == True:
-        #     if self.compare_item() > 0:
-        #         self.swap_item()
-        #         self.set_light_off()
-        # # Go back down the list until you find none again.
-        # while self.move_left() and not self.compare_item() == None:
-        #     if self.compare_item() > 0:
-        #         self.swap_item()
-        # self.swap_item()
-
-        # Pick up the first item
+        # Pick up first item
         self.swap_item()
         while True:
-        # Carry it to the end, swapping for bigger as you go
+            # While robot can move right
             while self.can_move_right():
+                # Move right
                 self.move_right()
+                # If you're not carrying anything, pick up the item in front of you
                 if self.compare_item() == None:
                     self.swap_item()
                     self.move_right()
-                if self.compare_item() < 1:
+                # Compare items. If held item is less, swap.
+                if self.compare_item() < 0 and self.can_move_right():
                     self.swap_item()
-        # Turn on light
-            self.set_light_on()
-        # Find first value smaller than carried value, swap and turn off light
-            while self.can_move_left() and self.light_is_on():
+            # While robot can move left
+            while self.can_move_left() or self.compare_item() == None:
+                # If there's an empty space put the thing you're carrying in it
+                if self.compare_item() == None:
+                    self.swap_item()
+                    break
+                # Compare items. If held item is more, swap.
                 if self.compare_item() > 0:
                     self.swap_item()
-                    self.set_light_off()
+                # Move left
                 self.move_left()
-        # Carry swapped value to 'None', swapping for smaller as you go, swap
-            while self.can_move_left() and self.compare_item() != None:
-                if self.compare_item() > 0:
-                    self.swap_item()
-                self.move_left()
-            self.swap_item()
-        # Move one right, pick up item, carry to end swapping for bigger as you go (step 2)
-        # End when light is off and the last value swapped was just to the left of the previous one
+            
         pass
 
 
@@ -165,7 +129,7 @@ if __name__ == "__main__":
     # Test our your implementation from the command line
     # with `python robot_sort.py`
 
-    l = [15, 41, 58, 49, 4, 26]
+    l = [5,4,3,2,1]
 
     robot = SortingRobot(l)
 
